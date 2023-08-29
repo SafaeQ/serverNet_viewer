ad7b9eada43f993f3541d3ef20b654e4b4723fd2
SafaeQ <safaedani009@gmail.com>
2023-08-29 12:37:19 +0100

    ⚡️ add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerListComponent } from './server-list/server-list.component';
+import { ServerDetailsComponent } from './server-details/server-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerListComponent,
+    ServerDetailsComponent
   ],
   imports: [
     BrowserModule
